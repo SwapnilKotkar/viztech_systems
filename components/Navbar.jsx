35621247136f17150e48d68b6b69454c9abbebc5
swapnil <43784955+SwapnilKotkar@users.noreply.github.com>
2022-11-09 20:42:06 +0530

    Site logo updated

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import NavLogo from "./NavLogo";
 
 const Navbar = () => {
   const router = useRouter();
@@ -32,18 +33,9 @@ const Navbar = () => {
           className="inline-flex items-center text-black-800 text-2xl md:text-3xl font-bold gap-2.5"
           aria-label="logo"
         >
-          <svg
-            width="95"
-            height="94"
-            viewBox="0 0 95 94"
-            className="w-6 h-auto text-indigo-500"
-            fill="currentColor"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M96 0V47L48 94H0V47L48 0H96Z" />
-          </svg>
-
-          <p className="text-xl">Viztech Systems</p>
+          <span>
+            <NavLogo color={"#3E1FFF"} height={30} />
+          </span>
         </Link>
 
         <nav className="hidden lg:flex gap-12">
